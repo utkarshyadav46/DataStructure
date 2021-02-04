65118401b77e3990282c852444c349d2d6158762
Utkarsh Yadav <37462832+utkarshyadav46@users.noreply.github.com>
2021-02-04 23:42:50 +0530

    Add files via upload

---
+++ Reverse a Linked List in groups of given size.cpp
@@ -0,0 +1,113 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct node *reverse (struct node *head, int k);
+
+struct node
+{
+    int data;
+    struct node* next;
+    
+    node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+/* Function to print linked list */
+void printList(struct node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+}
+
+/* Drier program to test above function*/
+int main(void)
+{
+    int t;
+    cin>>t;
+     
+    while(t--)
+    {
+        struct node* head = NULL;
+        struct node* temp = NULL;
+        int n;
+        cin >> n;
+         
+        for(int i=0 ; i<n ; i++)
+        {
+            int value;
+            cin >> value;
+            if(i == 0)
+            {
+                head = new node(value);
+                temp = head;
+            }
+            else
+            {
+                temp->next = new node(value);
+                temp = temp->next;
+            }
+        }
+        
+        int k;
+        cin>>k;
+        
+        head = reverse(head, k);
+        printList(head);
+    }
+     
+    return(0);
+}
+
+// } Driver Code Ends
+
+
+/*
+  Reverse a linked list
+  The input list will have at least one element  
+  Return the node which points to the head of the new LinkedList
+  Node is defined as 
+    struct node
+    {
+        int data;
+        struct node* next;
+    
+        node(int x){
+            data = x;
+            next = NULL;
+        }
+    
+    }*head;
+*/
+
+struct node *reverse (struct node *head, int k)
+{ 
+ struct node *cur=head;
+ struct node *prev=NULL;
+ struct node *next=NULL;
+    int c=0;
+    while(c<k && cur!=NULL)
+    {
+        next=cur->next;
+        cur->next=prev;
+        prev=cur;
+        cur=next;
+        c++;
+    }
+    
+    if(next!=NULL)
+    {
+        head->next=reverse(next,k);
+    }
+    
+    return prev;
+    // Complete this method
+}
+
